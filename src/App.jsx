4c15e8172a9b12c61c69597efd0e6889a268282a
Chandrakant Mishra <cm237575@gmail.com>
2024-08-08 23:42:15 +0530

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Employee from "./components/EmpTable"
+import Navebar from "./components/Navbar"
+
+
+function App() {
+
+  return (
+    <>
+      <Navebar />
+      <Employee />
+    </>
+  )
+}
+
+export default App
